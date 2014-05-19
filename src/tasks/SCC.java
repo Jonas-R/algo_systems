d108a7cf2a80853ad6c5f8cdfe9f50d0e8d57d2d
Jonas-R <jonas.raedle@gmail.com>
2014-05-19 15:10:22 +0200

    changed output formachanged output formatt

--- src/tasks/SCC.java
+++ src/tasks/SCC.java
@@ -2,45 +2,60 @@ package tasks;
 
 import graph.Graph;
 import graph.Node;
-import io.printGraph;
 import io.readGraph;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import utils.FileUtils;
 
-public class SSC {
+public class SCC {
+
     public static void main(String[] args) {
         if (args.length == 5) {
-        
+
             Graph G = readGraph.readAdjacencyListGraph("testgraph_ue05.txt");
 
-            String[] output_array = calculateSSC(G);
-            String output = "";
-            for (String s : output_array) { output += s + "\n"; }
-            if (!args[4].endsWith("/")) { args[4] += "/"; }
-            FileUtils.writeString(args[4] + "components.txt", output);
+            TreeMap<Integer, ArrayList<String>> output_map = calculateSCC(G);
+            StringBuilder output = new StringBuilder();
+            int i = 0;
+            for (ArrayList<String> comp : output_map.values()) {
+                output.append("component ").append(i).append(": ");
+                Collections.sort(comp);
+                String delim = "";
+                for (String node : comp) {
+                    output.append(delim).append(node);
+                    delim = ", ";
+                }
+                output.append('\n');
+                i++;
+            }
+
+            if (!args[4].endsWith("/")) {
+                args[4] += "/";
+            }
+            FileUtils.writeString(args[4] + "components.txt", output.toString());
         } else {
             System.out.println("Aufruf: schmidt_raedle_project.jar 3 -file <adjazenz_file> -o <ausgabe-ordner>");
         }
     }
-    
-    public static String[] calculateSSC(Graph G) {
+
+    public static TreeMap<Integer, ArrayList<String>> calculateSCC(Graph G) {
         int N = 0;
         Set<String> R = new HashSet<>();
         Map<String, Integer> components = new HashMap<>();
         Map<Integer, Node> phi = new HashMap<>();
-        
+
         for (Node node : G.getNodes()) {
             if (!R.contains(node.id)) {
                 N = dfs1(node, R, N, phi);
             }
         }
-        
+
         R = new HashSet<>();
         int K = 0;
         for (int i = G.getNodes().size(); i > 0; i--) {
@@ -49,21 +64,19 @@ public class SSC {
                 dfs2(G, phi.get(i), K, components, R);
             }
         }
-        
-        String[] out_components = new String[K];
-        
-        for (int i = 0; i < K; i++) {
-            out_components[i] = "comp " + i + ": ";
-            for (String key : components.keySet()) {
-                if (components.get(key) == i + 1) {
-                    out_components[i] += (key + 1) + " ";
-                } 
+
+        TreeMap<Integer, ArrayList<String>> out_components = new TreeMap<>();
+
+        for (String key : components.keySet()) {
+            if (!out_components.containsKey(components.get(key))) {
+                out_components.put(components.get(key), new ArrayList<String>());
             }
+            out_components.get(components.get(key)).add(key);
         }
-        
+
         return out_components;
     }
-    
+
     public static int dfs1(Node node, Set<String> R, int N, Map<Integer, Node> phi) {
         R.add(node.id);
         for (SimpleEntry<Node, String> edge : node.edges) {
@@ -71,10 +84,11 @@ public class SSC {
                 N = dfs1(edge.getKey(), R, N, phi);
             }
         }
-        N++; phi.put(N, node);
+        N++;
+        phi.put(N, node);
         return N;
     }
-    
+
     public static void dfs2(Graph G, Node node, int K, Map<String, Integer> components, Set<String> R) {
         R.add(node.id);
         for (Node other_node : G.getNodes()) {
@@ -86,6 +100,4 @@ public class SSC {
         }
         components.put(node.id, K);
     }
-    
-
 }
